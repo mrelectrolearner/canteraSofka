d738399756e338194fd2eb47fa0fe4686eb4e6a9
luis <luisfelorivas@gmai.com>
2022-02-10 23:00:00 -0500

    update Spacecraft.java: change float->double and add methods.

--- week1/taller2/src/taller2/Spacecraft.java
+++ week1/taller2/src/taller2/Spacecraft.java
@@ -1,7 +1,7 @@
 package taller2;
 
 /**
- * Represent the spacecraft and its basic operation using the launch site
+ * Represent the spacecraft and its basic actions using the launch site
  * as inertial reference system and mks unit for measurement.
  *
  * @author  Luis Felipe Rivas
@@ -10,11 +10,11 @@ public abstract class Spacecraft {
     /**
      * Represent the spacecraft's speed.
      */
-    private float[] speed;
+    private double[] speed;
     /**
-     * Represent the spacecraft's power.
+     * Represent the power consumed by the spacecraft.
      */
-    private float power;
+    private double power;
     /**
      * Represent the spacecraft's name.
      */
@@ -22,7 +22,7 @@ public abstract class Spacecraft {
     /**
      * Represent the spacecraft's position.
      */
-    private float[] position;
+    private double[] position;
     /**
      * Represent the spacecraft's type of propulsion system.
      */
@@ -37,39 +37,86 @@ public abstract class Spacecraft {
     private Fuel fuel ;
 
     /**
-     * Instance a spacecraft turn off and with a position in {0,0,0}
-     * @param speed spacecraft's speed.
+     * Instance a spacecraft turn off and with a position in {0,0,0} and speed {0,0,0}.
      * @param power spacecraft's power.
      * @param name spacecraft's name.
      * @param typeOfPropulsionSystem  spacecraft's type of propulsion system.
      * @param fuel  spacecraft's fuel.
      */
-    public Spacecraft(float[] speed, float power, String name, String typeOfPropulsionSystem, Fuel fuel) {
-        this.speed = speed;
+    public Spacecraft( double power, String name, String typeOfPropulsionSystem, Fuel fuel) {
+        this.speed = new double[]{0,0,0};
         this.power = power;
         this.name = name;
         this.fuel = fuel;
-        this.position = new float[]{0, 0, 0};
+        this.position = new double[]{0, 0, 0};
         this.typeOfPropulsionSystem = typeOfPropulsionSystem;
         this.state =false;
 
     }
 
     /**
-     * land the spacecraft.
-     */
-    public abstract void land();
-
-    /**
-     * Move the spacecraft  a distance
-     * @param distance the vector distance that is moved the spacecraft.
-     */
-    public abstract void move(float [] distance);
-
-    /**
      * Speed up the spacecraft with an acceleration.
      * @param acceleration the acceleration thar is used to speed up the spacecraft.
      */
-    public abstract void speedUp(float[] acceleration);
+    public abstract void speedUp(double[] acceleration);
+
+    public abstract double distanceToPower(double distance);
+
+
+    /**
+     * Calculates the fuel consumption for a power
+     * @param power the power use for the spacecraft.
+     * @return the fuel consumed.
+     */
+    public abstract double powerToFuel(double power);
+
+    /**
+     * fuel consumption
+     * @param power the power use for the spacecraft
+     */
+    public void fuelConsumed(double power){
+        double fuelConsumed=this.powerToFuel(power);
+        this.fuel.setVolume(this.fuel.getVolume()-fuelConsumed);
+    }
+
+    /**
+     * Land the spacecraft.
+     */
+    public void land(){
+        boolean land=this.position.equals(new double[]{0,0,0});
+        double minDistance=0.001;
+        double minPowerConsumed=this.distanceToPower(minDistance);
+        while(!land){
+            for (int index = 0; index <position.length; index++){
+                if(this.position[index]>0){
+                    this.fuelConsumed(minPowerConsumed);
+                    this.position[index]-=minDistance;
+                }else if (this.position[index]<0){
+                    this.fuelConsumed(minPowerConsumed);
+                    this.position[index]+=minDistance;
+                }
+            }
+        }
+    };
+
+    /**
+     * Move the spacecraft a distance
+     * @param distance the vector distance that is moved the spacecraft.
+     */
+    public void move(double [] distance){
+        double powerConsumed=0;
+        for (int index = 0; index <position.length; index++){
+            powerConsumed=this.distanceToPower(distance[index]);
+            this.fuelConsumed(powerConsumed);
+            this.position[index]+=distance[index];
+        }
+
+    };
+
+
+
+
+
+
 
 }
