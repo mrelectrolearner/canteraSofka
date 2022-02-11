df5cd85e8c601616c5f4090138cfc95c41c12ebc
luis <luisfelorivas@gmai.com>
2022-02-10 20:13:50 -0500

    update Spacecraft.java: change attributes and add the documentation.

--- week1/taller2/src/taller2/Spacecraft.java
+++ week1/taller2/src/taller2/Spacecraft.java
@@ -1,17 +1,49 @@
 package taller2;
 
-import taller2.Fuel;
-
+/**
+ * Represent the spacecraft and its basic operation using the launch site
+ * as inertial reference system and mks unit for measurement.
+ *
+ * @author  Luis Felipe Rivas
+ */
 public abstract class Spacecraft {
-    private static float outOfEarth=1000000;
-    private float speed;
+    /**
+     * Represent the spacecraft's speed.
+     */
+    private float[] speed;
+    /**
+     * Represent the spacecraft's power.
+     */
     private float power;
+    /**
+     * Represent the spacecraft's name.
+     */
     private String name;
+    /**
+     * Represent the spacecraft's position.
+     */
     private float[] position;
+    /**
+     * Represent the spacecraft's type of propulsion system.
+     */
     private String typeOfPropulsionSystem;
+    /**
+     * Represent if the spacecraft is on.
+     */
     public boolean state;
+    /**
+     * Represent the spacecraft's fuel.
+     */
     private Fuel fuel ;
 
+    /**
+     * Instance a spacecraft turn off and with a position in {0,0,0}
+     * @param speed spacecraft's speed.
+     * @param power spacecraft's power.
+     * @param name spacecraft's name.
+     * @param typeOfPropulsionSystem  spacecraft's type of propulsion system.
+     * @param fuel  spacecraft's fuel.
+     */
     public Spacecraft(float speed, float power, String name, String typeOfPropulsionSystem, Fuel fuel) {
         this.speed = speed;
         this.power = power;
@@ -23,10 +55,21 @@ public abstract class Spacecraft {
 
     }
 
+    /**
+     * land the spacecraft.
+     */
     public abstract void land();
-    public abstract void moveX(float distance);
-    public abstract void moveY(float distance);
-    public abstract void moveZ(float distance);
-    public abstract void speedUp(float acceleration);
+
+    /**
+     * Move the spacecraft  a distance
+     * @param distance the vector distance that is moved the spacecraft.
+     */
+    public abstract void move(float [] distance);
+
+    /**
+     * Speed up the spacecraft with an acceleration.
+     * @param acceleration the acceleration thar is used to speed up the spacecraft.
+     */
+    public abstract void speedUp(float[] acceleration);
 
 }
