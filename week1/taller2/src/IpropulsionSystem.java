09ee910154daf3d4d334b75d8c44d95ddfa3c0db
luis <luisfelorivas@gmai.com>
2022-02-10 19:48:18 -0500

    update IpropulsionSystem.java: the documentation is added.

--- week1/taller2/src/IpropulsionSystem.java
+++ week1/taller2/src/IpropulsionSystem.java
@@ -1,8 +1,24 @@
+/**
+ * Represent the propulsion System interface
+ * @author Luis Felipe Rivas
+ */
 public interface IpropulsionSystem {
-
-
+    /**
+     * Activate the propulsion system from lef to right.
+     * @param power the power of the propulsion.
+     */
     void propulsionPowerX(float power);
+
+    /**
+     * Activate the propulsion system from down to up.
+     * @param power the power of the propulsion.
+     */
     void propulsionPowerY(float power);
+
+    /**
+     * Activate the propulsion system back to forward.
+     * @param power the power of the propulsion.
+     */
     void propulsionPowerZ(float power);
 
 }
