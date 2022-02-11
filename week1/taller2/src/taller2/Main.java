302f58c3ee6b3341524080a9f85906c250ab7e9b
luis <luisfelorivas@gmai.com>
2022-02-11 18:54:28 -0500

    update classes, interface.

--- week1/taller2/src/taller2/Main.java
+++ week1/taller2/src/taller2/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * Is the interface with the user and its operations.
+ * Is the interface for the user and its operations.
  */
 public class Main {
    public static void main(String[] args) {
