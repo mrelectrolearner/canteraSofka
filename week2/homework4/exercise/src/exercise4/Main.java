71d715e42447702e91e27656d476426ce825764b
luis <luisfelorivas@gmai.com>
2022-02-14 18:01:57 -0500

    update exercise4: add exceptions treatment and documentations.

--- week2/homework4/exercise/src/exercise4/Main.java
+++ week2/homework4/exercise/src/exercise4/Main.java
@@ -4,33 +4,74 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
 
+/**
+ * Represent the interface with the user and its interactions.
+ * @version 1.0.0 2022-02-15.
+ * @author Luis Felipe Rivas.
+ * @since 1.0.0 2022-02-15.
+ */
 public class Main {
     public static void main(String[] args) {
-        int passangersNumber;
+        int passengersNumber;
         boolean thereAreCrew;
-        int wheelesNumber;
-        Calendar registerDate=new GregorianCalendar(0,0,0);
-        String travelMeans;
-        Scanner scanner=new Scanner(System.in);
+        int wheelsNumber;
+        int counter=0;
+        while (counter<10) {
+            try {
+                Calendar registerDate = new GregorianCalendar();
+                String travelMeans;
+                Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome, for creating a new vehicule please: \n");
-        System.out.println("Enter the passangerNumber: ");
-        passangersNumber=scanner.nextInt();
-        System.out.println("Enter is thereAreCrew ");
-        thereAreCrew=scanner.nextBoolean();
-        System.out.println("Enter is wheelesNumber ");
-        wheelesNumber=scanner.nextInt();
-        System.out.println("Enter the register year: ");
-        registerDate.add(Calendar.YEAR,scanner.nextInt());
-        System.out.println("Enter the register month: ");
-        registerDate.add(Calendar.MONTH,scanner.nextInt());
-        System.out.println("Enter the register day: ");
-        registerDate.add(Calendar.DAY_OF_MONTH,scanner.nextInt());
-        System.out.println("Enter the travel means: ");
-        travelMeans=scanner.next();
-        Vehicle vehicle=new Vehicle(passangersNumber,thereAreCrew,wheelesNumber,registerDate,travelMeans);
-        System.out.println("A new Vehicle was created\n");
-        System.out.print(vehicle);
+                System.out.println("Welcome, for creating a new vehicle please: \n");
+                System.out.println("Enter the passengerNumber: ");
+                passengersNumber = scanner.nextInt();
+                if(passengersNumber <0){
+                    throw new IllegalArgumentException("Negative  passenger number is not allowed.");
+                }
+                System.out.println("Enter is thereAreCrew ");
+                thereAreCrew = scanner.nextBoolean();
+                System.out.println("Enter is wheelsNumber ");
+                wheelsNumber = scanner.nextInt();
+                if(wheelsNumber <0){
+                    throw new IllegalArgumentException("Negative wheels number is not allowed.");
+                }
+                System.out.println("Enter the register year: ");
+                int years=scanner.nextInt();
+
+                if(years <0){
+                    throw new IllegalArgumentException("Negative year is not allowed.");
+                }
+                registerDate.add(Calendar.YEAR,years );
+                System.out.println("Enter the register month: ");
+                int month =scanner.nextInt();
+                if(month <0){
+                    throw new IllegalArgumentException("Negative month is not allowed.");
+                }
+                registerDate.add(Calendar.MONTH, month);
+                System.out.println("Enter the register day: ");
+                int day =scanner.nextInt();
+                if(day <0){
+                    throw new IllegalArgumentException("Negative day is not allowed.");
+                }
+                registerDate.add(Calendar.DAY_OF_MONTH, day);
+                System.out.println("Enter the travel means: ");
+                travelMeans = scanner.next();
+                Vehicle vehicle = new Vehicle(passengersNumber,
+                        thereAreCrew, wheelsNumber, registerDate, travelMeans);
+                System.out.println("A new Vehicle was created\n");
+                System.out.print(vehicle);
+                counter+=1;
+
+            }catch (IllegalArgumentException exc){
+                System.out.println("Error in the argument enter, " +
+                        "it was not possible create the vehicle.\n"+exc);
+            }catch (Exception exc){
+                System.out.println("Error, it was not possible create the vehicle.\n"+exc);
+
+            }
+
+
+        }
 
 
 
--- week2/homework4/exercise/src/exercise4/Vehicle.java
+++ week2/homework4/exercise/src/exercise4/Vehicle.java
@@ -2,90 +2,182 @@ package exercise4;
 
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Objects;
 
+/**
+ * Represent a vehicle and its basic operations.
+ * @version 1.0.0 2022-02-15.
+ * @author Luis Felipe Rivas.
+ * @since 1.0.0 2022-02-15.
+ */
 public class Vehicle {
-    private final int passangersNumber;
+    private final int passengersNumber;
     private final boolean thereAreCrew;
-    private final int wheelesNumber;
+    private final int wheelsNumber;
     private final Calendar registerDate;
     private final String travelMeans;
     private double[] position;
     private double[] velocity;
 
-    public Vehicle(
-            int passangersNumber,
+    /**
+     * Instance a vehicle.
+     * @param passengersNumber the passengers number
+     * @param thereAreCrew represent is there are a crew.
+     * @param wheelsNumber the wheels number.
+     * @param registerDate the  vehicle's register date.
+     * @param travelMeans the vehicle's travel means.
+     */
+    public Vehicle(int passengersNumber,
             boolean thereAreCrew,
-            int wheelesNumber,
+            int wheelsNumber,
             Calendar registerDate,
-            String travelMeans) {
-        this.passangersNumber = passangersNumber;
+            String travelMeans)
+    {
+        this.passengersNumber = passengersNumber;
         this.thereAreCrew = thereAreCrew;
-        this.wheelesNumber = wheelesNumber;
-        this.registerDate = registerDate;
-        this.travelMeans = travelMeans;
+        this.wheelsNumber = wheelsNumber;
+        this.registerDate = Objects.requireNonNull(registerDate);
+        this.travelMeans = Objects.requireNonNull(travelMeans);
         this.position = new double[3];
         this.velocity = new double[3];
     }
 
-    private double[] addArray(double [] array1, double[] array2){
-        double [] sum=new double[3];
-        for (int index=0; index<array1.length; index++) {
-            sum[index]=array1[index]+array2[index];
+    /**
+     * add two arrays of doubles of tree dimensions.
+     * @param array1 the first array to add.
+     * @param array2 the second array to add.
+     * @return the addition of the first and second array.
+     */
+
+    private double[] addArrays(double [] array1, double[] array2){
+        double[] sum = new double[3];
+        for (int index = 0; index < array1.length; index++) {
+            sum[index] = array1[index] + array2[index];
         }
         return sum;
+
     }
 
-    public double[] move(double[] distance){
-        return this.addArray(this.getPosition(),distance);
+    /**
+     *  Change the position of a vehicle a distance.
+     * @param distance the distance to move the vehicle.
+     */
+
+    public void move(double[] distance){
+        try {
+           this.setPosition(this.addArrays(this.getPosition(), distance));
+        }catch (ArrayIndexOutOfBoundsException exc ){
+            System.out.println("Array out of index exception in the position addition:\n"+exc);
+
+
+        }
     }
 
-    public double[] speedup(double[] acceleration){
-        return this.addArray(this.getVelocity(), acceleration);
+    /**
+     * Change the velocity of the vehicle.
+     * @param acceleration the amount that change the velocity.
+     */
+    public void speedup(double[] acceleration){
+        try {
+            this.setVelocity(this.addArrays(this.getVelocity(), acceleration));
+        }catch (ArrayIndexOutOfBoundsException exc){
+            System.out.println("Array out of index exception in the velocity addition:\n"+exc);
+
+        }
     }
 
-    public int getPassangersNumber() {
-        return passangersNumber;
+    /**
+     * Get the passengers number.
+     * @return the passengers number.
+     */
+    public int getPassengersNumber() {
+        return passengersNumber;
     }
 
-    public boolean isThereAreCrew() {
+    /**
+     * Get if the vehicle  are crewed.
+     * @return if vehicle are crewed.
+     */
+
+    public boolean ThereAreCrew() {
         return thereAreCrew;
     }
 
-    public int getWheelesNumber() {
-        return wheelesNumber;
+    /**
+     * Get the wheels number.
+     * @return the wheels number.
+     */
+
+    public int getWheelsNumber() {
+        return wheelsNumber;
     }
+
+    /**
+     * Get the vehicle's register Date.
+     * @return vehicle's register Date.
+     */
 
     public Calendar getRegisterDate() {
         return registerDate;
     }
 
+    /**
+     * Get the vehicle's travel means.
+     * @return vehicle's travel means.
+     */
+
     public String getTravelMeans() {
         return travelMeans;
     }
+
+    /**
+     * Get the vehicle's position.
+     * @return vehicle's position.
+     */
 
     public double[] getPosition() {
         return position;
     }
 
+    /**
+     * Set the vehicle's position.
+     * @param position the vehicle's position.
+     */
+
     public void setPosition(double[] position) {
-        this.position = position;
+        this.position = Objects.requireNonNull(position);
     }
+
+    /**
+     * Get the velocity.
+     * @return the velocity.
+     */
 
     public double[] getVelocity() {
         return velocity;
     }
 
+    /**
+     * Set the vehicle's velocity.
+     * @param velocity the vehicle's velocity.
+     */
+
     public void setVelocity(double[] velocity) {
-        this.velocity = velocity;
+        this.velocity = Objects.requireNonNull(velocity);
     }
+
+    /**
+     * Catch in a string the information of the vehicle.
+     * @return the vehicle's information.
+     */
 
     @Override
     public String toString() {
         return "Vehicle{" +
-                "passangersNumber=" + passangersNumber +
+                "passengersNumber=" + passengersNumber +
                 ", thereAreCrew=" + thereAreCrew +
-                ", wheelesNumber=" + wheelesNumber +
-                ", registerDate=" +  + registerDate.get(Calendar.DAY_OF_MONTH)+"/"
+                ", wheelsNumber=" + wheelsNumber +
+                ", registerDate=" +   registerDate.get(Calendar.DAY_OF_MONTH)+"/"
                 + registerDate.get(Calendar.MONTH)+"/"+ registerDate.get(Calendar.YEAR)+
                 ", travelMeans='" + travelMeans + '\'' +
                 ", position=" + Arrays.toString(position) +
