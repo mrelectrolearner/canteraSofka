a3e277f4a9c3f1f056568cd3eb7bb66133574dcd
luis <luisfelorivas@gmai.com>
2022-02-16 21:48:26 -0500

    update Exercise6.java

--- week2/homework4/exercise/src/exercise6/Exercise6.java
+++ week2/homework4/exercise/src/exercise6/Exercise6.java
@@ -3,20 +3,40 @@ package exercise6;
 import java.util.Scanner;
 import java.util.Vector;
 
+/**
+ * Represent a list of number enter by the user until its enter two numbers equals next to each other.
+ * @author Luis Felipe Rivas -luisfelorivas@gmail.com.
+ * @since 1.0.0 2022-02-15.
+ */
 public class Exercise6 {
+    /**
+     * Ask the user a number and save in a list until it receive to two equals number next to each other.
+     * @author Luis Felipe Rivas -luisfelorivas@gmail.com.
+     * @since 1.0.0 2022-02-15.
+     * @param args
+     */
     public static void main(String[] args) {
-        Vector numberList=new Vector<>();
+        Vector numberList=new Vector<>(0);
         Scanner scanner=new Scanner(System.in);
         System.out.println("Welcome!!");
-        boolean isNumberInTheList =false;
-        while(!isNumberInTheList) {
-            System.out.println("\nPlease, enter a number: ");
-            Double enterNumber=scanner.nextDouble();
-            isNumberInTheList=numberList.contains(enterNumber);
-            numberList.add(enterNumber);
-            System.out.println("the number " + enterNumber + " was added to the list\n");
+        boolean isEqualToTheLastNumber =false;
+        while(!isEqualToTheLastNumber) {
+            try {
+                System.out.println("\nPlease, enter a number: ");
+                Double enterNumber=scanner.nextDouble();
+                if(numberList.size()>0){
+                    Double lasElement= (Double) numberList.lastElement();
+                    isEqualToTheLastNumber =enterNumber.equals(lasElement);
+                }
+                numberList.add(enterNumber);
+                System.out.println("the number " + enterNumber + " was added to the list\n");
+            }catch (Exception exc){
+                System.out.println("Error! "+ exc+" try again\n");
+
+            }
+
         }
 
-        System.out.println("The number entered were: "+numberList);
+        System.out.println("The numbers entered were: "+numberList);
     }
 }
